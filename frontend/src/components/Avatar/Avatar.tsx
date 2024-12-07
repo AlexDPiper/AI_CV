a08198b5f2ddbe2e706b15c8ca1052b6cb5b2999
Alexander <alexanderdudarev.music@gmail.com>
2024-12-07 16:13:19 +0100

    Updated 3d model with morphtargets and visemes. Added eye blink, facial expression logic

--- frontend/src/components/Avatar/Avatar.tsx
+++ frontend/src/components/Avatar/Avatar.tsx
@@ -1,13 +1,95 @@
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useAnimations, useGLTF } from '@react-three/drei';
-import { FC, useEffect, useMemo, useRef, useState } from 'react';
-import { Group, Object3DEventMap } from 'three';
+import { useFrame } from '@react-three/fiber';
+import {
+	Group,
+	MathUtils,
+	Object3D,
+	Object3DEventMap,
+	SkinnedMesh,
+} from 'three';
 
 import { GLTFResult } from './types';
+
+const facialExpressions: Record<string, { [key: string]: number }> = {
+	default: {},
+	smile: {
+		browInnerUp: 0.17,
+		eyeSquintLeft: 0.4,
+		eyeSquintRight: 0.44,
+		noseSneerLeft: 0.1700000727403593,
+		noseSneerRight: 0.14000002836874015,
+		mouthPressLeft: 0.61,
+		mouthPressRight: 0.41000000000000003,
+	},
+	funnyFace: {
+		jawLeft: 0.63,
+		mouthPucker: 0.53,
+		noseSneerLeft: 1,
+		noseSneerRight: 0.39,
+		mouthLeft: 1,
+		eyeLookUpLeft: 1,
+		eyeLookUpRight: 1,
+		cheekPuff: 0.9999924982764238,
+		mouthDimpleLeft: 0.414743888682652,
+		mouthRollLower: 0.32,
+		mouthSmileLeft: 0.35499733688813034,
+		mouthSmileRight: 0.35499733688813034,
+	},
+	sad: {
+		mouthFrownLeft: 1,
+		mouthFrownRight: 1,
+		mouthShrugLower: 0.78341,
+		browInnerUp: 0.452,
+		eyeSquintLeft: 0.72,
+		eyeSquintRight: 0.75,
+		eyeLookDownLeft: 0.5,
+		eyeLookDownRight: 0.5,
+		jawForward: 1,
+	},
+	surprised: {
+		eyeWideLeft: 0.5,
+		eyeWideRight: 0.5,
+		jawOpen: 0.351,
+		mouthFunnel: 1,
+		browInnerUp: 1,
+	},
+	crazy: {
+		browInnerUp: 0.9,
+		jawForward: 1,
+		noseSneerLeft: 0.5700000000000001,
+		noseSneerRight: 0.51,
+		eyeLookDownLeft: 0.39435766259644545,
+		eyeLookUpRight: 0.4039761421719682,
+		eyeLookInLeft: 0.9618479575523053,
+		eyeLookInRight: 0.9618479575523053,
+		jawOpen: 0.9618479575523053,
+		mouthDimpleLeft: 0.9618479575523053,
+		mouthDimpleRight: 0.9618479575523053,
+		mouthStretchLeft: 0.27893590769016857,
+		mouthStretchRight: 0.2885543872656917,
+		mouthSmileLeft: 0.5578718153803371,
+		mouthSmileRight: 0.38473918302092225,
+		tongueOut: 0.9618479575523053,
+	},
+};
+
+const visemesMap = {
+	A: 'viseme_PP',
+	B: 'viseme_kk',
+	C: 'viseme_I',
+	D: 'viseme_AA',
+	E: 'viseme_O',
+	F: 'viseme_U',
+	G: 'viseme_FF',
+	H: 'viseme_TH',
+	X: 'viseme_PP',
+};
 
 type AvatarProps = JSX.IntrinsicElements['group'];
 
 export const Avatar: FC<AvatarProps> = (props) => {
-	const { nodes, materials } = useGLTF(
+	const { nodes, materials, scene } = useGLTF(
 		'/models/674a4cb0565cd4473b6688b9.glb'
 	) as GLTFResult;
 
@@ -15,7 +97,7 @@ export const Avatar: FC<AvatarProps> = (props) => {
 
 	const groupRef = useRef<Group<Object3DEventMap>>(null);
 
-	const { actions, mixer } = useAnimations(animations, groupRef);
+	const { actions } = useAnimations(animations, groupRef);
 
 	const initialAnimation = useMemo(
 		() =>
@@ -25,7 +107,7 @@ export const Avatar: FC<AvatarProps> = (props) => {
 		[animations]
 	);
 
-	const [animation, setAnimation] = useState(animations[8].name);
+	const [animation, setAnimation] = useState(initialAnimation);
 
 	useEffect(() => {
 		actions[animation]?.reset().fadeIn(0.5).play();
@@ -34,6 +116,74 @@ export const Avatar: FC<AvatarProps> = (props) => {
 			actions[animation]?.fadeOut(0.5);
 		};
 	}, [animation]);
+
+	const [blink, setBlink] = useState(false);
+	const [facialExpressionName, setFacialExpressionName] = useState('');
+
+	useEffect(() => {
+		let blinkTimeout: number;
+
+		const nextBlink = () => {
+			blinkTimeout = setTimeout(
+				() => {
+					setBlink(true);
+					setTimeout(() => {
+						setBlink(false);
+						nextBlink();
+					}, 200);
+				},
+				MathUtils.randInt(1000, 5000)
+			);
+		};
+
+		nextBlink();
+
+		return () => clearTimeout(blinkTimeout);
+	}, []);
+
+	const lerpMorphTarget = useCallback(
+		(target: string, value: number, speed = 0.1) => {
+			scene.traverse((child: Object3D<Object3DEventMap>) => {
+				if (child instanceof SkinnedMesh) {
+					if (child.isSkinnedMesh && child.morphTargetDictionary) {
+						const index = child.morphTargetDictionary[target];
+						if (
+							index === undefined ||
+							child.morphTargetInfluences?.[index] === undefined
+						) {
+							return;
+						}
+
+						child.morphTargetInfluences[index] = MathUtils.lerp(
+							child.morphTargetInfluences?.[index],
+							value,
+							speed
+						);
+					}
+				}
+			});
+		},
+		[scene]
+	);
+
+	useFrame(() => {
+		if (nodes.EyeLeft.morphTargetDictionary) {
+			Object.keys(nodes.EyeLeft.morphTargetDictionary).forEach((key) => {
+				const mapping = facialExpressions[facialExpressionName];
+				if (key === 'eyeBlinkLeft' || key === 'eyeBlinkRight') {
+					return;
+				}
+				if (mapping && mapping[key]) {
+					lerpMorphTarget(key, mapping[key], 0.1);
+				} else {
+					lerpMorphTarget(key, 0, 0.1);
+				}
+			});
+		}
+
+		lerpMorphTarget('eyeBlinkLeft', blink ? 1 : 0, 0.5);
+		lerpMorphTarget('eyeBlinkRight', blink ? 1 : 0, 0.5);
+	});
 
 	return (
 		<group {...props} dispose={null} ref={groupRef}>
